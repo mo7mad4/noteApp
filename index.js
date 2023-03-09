19fe348f3c8495f814a4f22911c5708443fff4cf
Mohammed Zeyad Abu Raida <mohammedreda814@gmail.com>
2023-03-09 12:53:14 +0200

    Adding code userRoutes

--- index.js
+++ index.js
@@ -1,8 +1,13 @@
 const express = require('express');
+const cors = require('cors');
 const { connection } = require('./db');
+const { userRouter } = require('./routes/user.routes');
 require('dotenv').config()
 const port = process.env.PORT
-const app = express()
+const app = express();
+app.use(cors())
+app.use(express.json())
+app.use("/user", userRouter)
 
 app.get("/", (req, res) => {
     res.send({
